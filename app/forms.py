156eb63100617249223d734ad6503b78791b1c57
denys <mystalowawola@gmail.com>
2018-09-08 12:12:39 +0200

    login form works

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,9 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(object):
-    user_name = StringField(label='username', validators=DataRequired())
-    user_pswd = PasswordField(label='pswd', validators=DataRequired())
-    keep_logged = BooleanField(label='remember me')
-    submit = SubmitField(label='submit')
+class LoginForm(FlaskForm):
+    user_name = StringField(label='Username', validators=[DataRequired()])
+    user_pswd = PasswordField(label='Password', validators=[DataRequired()])
+    keep_logged = BooleanField(label='Remember')
+    submit = SubmitField(label='Submit')
 
